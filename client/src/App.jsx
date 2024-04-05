5b048e9ae0eb5cfce9330bb312feaf06ea242c4c
VaniyOstapenko <ostapenkovaniykalivan@gmail.com>
2024-04-05 16:29:27 +0300

    MNTN-Guide

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import Header from "./pages/Header/Header";
 import Main from "./pages/Main/Main";
 import Content from "./pages/Content/Content";
+import Footer from "./pages/Footer/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header></Header>
       <Main></Main>
       <Content></Content>
+      <Footer></Footer>
     </>
   );
 }
